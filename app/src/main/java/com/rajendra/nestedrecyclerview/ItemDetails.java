579d1dddd531fd3183a725f5600764bcd38c87ac
Rishk3 <anchalesh10@gmail.com>
2020-09-19 00:16:50 +0530

    Item Details added

---
+++ app/src/main/java/com/rajendra/nestedrecyclerview/ItemDetails.java
@@ -0,0 +1,13 @@
+package com.rajendra.nestedrecyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ItemDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_details);
+    }
+}
